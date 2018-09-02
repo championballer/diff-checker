855b497569792ec50e0acc143a78afdae7738580
Shiv Kumar <championballer08@gmail.com>
2018-09-02 12:21:14 +0530

    changes made

--- main.cpp
+++ main.cpp
@@ -5,9 +5,32 @@ using namespace std;
 
 int main()
 {
+	string oname;
+	cout<<"Please add name for output file:";
+	cin>>oname;
 	
-	ofstream outfile("sample.txt",ios::out);
-	outfile<<"Hello, I am Iron Man"<<endl;
+	ofstream outfile(oname.c_str(),ios::out);
+	outfile<<"Hello, I am Iron Man\nThis is my file"<<endl;
 	outfile.close();
 	
+	ifstream infile;
+	infile.open("sample.txt");
+	
+	char c;
+	string current="";
+	while(!infile.eof())
+	{
+		infile.get(c);
+		if(c=='\n')
+		{
+		     //cout<<current<<endl;
+		     current="";
+		     continue;
+		}
+		else current+=c;
+
+	}
+	
+	if(infile.eof())cout<<"End reached"<<endl;
+		
 }
